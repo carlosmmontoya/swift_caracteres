77b04162a0885f4500a9338966441bb28c7c1d80
carlosmmontoya <83318534+carlosmmontoya@users.noreply.github.com>
2022-10-03 16:26:56 +0200

    cuenta_caracteres
    funcion swift que cuenta los caracteres de una cadena

---
+++ cuentaCadena.swift
@@ -0,0 +1,19 @@
+
+    var cadi : Int = 0
+
+func sublen(cadea: String ) -> Int {
+    
+
+    for caracter in cadea {
+
+cadi=cadi + 1
+    
+}
+
+ return  cadi
+ 
+}
+
+
+var r: Int = sublen(cadea: "hola amor como estas yo aqui extrañadote hecho um lio como siempre mo puedo eterder lo que aveces haces " )
+print(r)
